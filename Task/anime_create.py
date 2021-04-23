585caf590ce2a7172f1eccdaed405d175e51452c
a20034294 <a20034294@gmail.com>
2021-04-23 15:33:05 +0800

    [style] Fix #4 use multiple line fstring instead of string concat

--- Task/anime_create.py
+++ Task/anime_create.py
@@ -3,17 +3,18 @@ from os import getenv as env
 
 
 def anime_create_task(src_path, dst_path):
-    script = 'ffmpeg -i "' + env('ANIME_SRC_ROOT_DIR') + src_path + '"'
-    script += ' -acodec copy'
-    script += ' -vcodec libx264'
-    script += ' -crf 19'
-    script += ' -map 0 -f segment'
-    script += ' -segment_time 5'
-    script += ' -segment_list "' + \
-        env('ANIME_DST_ROOT_DIR') + dst_path + '/playlist.m3u8"'
-    script += ' -segment_list_entry_prefix "' + dst_path + '/"'
-    script += ' "' + dst_path + '/output%04d.ts' + '"'
+    params = " -acodec copy" \
+        " -vcodec libx264" \
+        " -crf 19" \
+        " -map 0 -f segment" \
+        " -segment_time 5"
+
+    script = f"ffmpeg -i \"{env('ANIME_SRC_ROOT_DIR') + src_path}\"" \
+        f"{params}" \
+        f" -segment_list \"{env('ANIME_DST_ROOT_DIR') + dst_path}/playlist.m3u8\"" \
+        f" -segment_list_entry_prefix \"{dst_path}/\"" \
+        f" \"{dst_path}/output%04d.ts\""
 
     print(script)
     os.system(script)
-    return dst_path + '/playlist.m3u8'
+    return dst_path + "/playlist.m3u8"
