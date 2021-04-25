828f177426a899d55d9c5850ef4c076f885d43f9
a20034294 <a20034294@gmail.com>
2021-04-26 02:27:04 +0800

    [design] Add res arg to worker function Now can reuse code for generate multi resolution segment stream

--- Task/anime_create.py
+++ Task/anime_create.py
@@ -3,26 +3,32 @@ from os import getenv as env
 
 
 def anime_create_task(src_path, dst_path):
-    anime_create_task_h264_1080(src_path, dst_path)
+    ffmpeg_trans_hls(src_path, dst_path, 1080)
     return dst_path + "/playlist.m3u8"
 
 
-def anime_create_task_h264_1080(src_path, dst_path):
+def ffmpeg_trans_hls(src_path, dst_path, resolution=1080):
+    res = str(resolution)
     params = \
         " -acodec copy" \
         " -vcodec libx264" \
-        " -filter_complex \"[0:v]uspp[a]; [a]gradfun[b]; [b]scale=-1:1080:flags=lanczos+full_chroma_int\"" \
+        f" -filter_complex \"[0:v]uspp[a]; [a]gradfun[b]; [b]scale=-1:{res}:flags=lanczos+full_chroma_int\"" \
         " -crf 20" \
-        " -preset:v veryslow" \
+        " -preset:v slow" \
         " -map 0 -f segment" \
         " -segment_time 5"
 
+    input_file_name = os.path.join(env('ANIME_SRC_ROOT_DIR'), src_path)
+    output_m3u8_name = os.path.join(env('ANIME_DST_ROOT_DIR'), dst_path,
+                                    f"playlist_h264_{res}p_uspp.m3u8")
+    output_ts_name = os.path.join(env('ANIME_DST_ROOT_DIR'), dst_path,
+                                  f"output%04d_h264_{res}p_uspp.ts")
     script = \
-        f"ffmpeg -i \"{os.path.join(env('ANIME_SRC_ROOT_DIR'), src_path)}\"" \
+        f"ffmpeg -i \"{input_file_name}\"" \
         f"{params}" \
-        f" -segment_list \"{os.path.join(env('ANIME_DST_ROOT_DIR'), dst_path, 'playlist_h264_1080p_uspp.m3u8')}\"" \
+        f" -segment_list \"{output_m3u8_name}\"" \
         f" -segment_list_entry_prefix \"/{dst_path}/\"" \
-        f" \"{os.path.join(env('ANIME_DST_ROOT_DIR'), dst_path, 'output%04d_h264_1080p_uspp.ts')}\""
+        f" \"{output_ts_name}\""
 
     print(script)
     os.system(script)
