603721472834a31c115fe8e0d038b13ff44f8690
a20034294 <a20034294@gmail.com>
2021-05-11 13:00:48 +0800

    [feat] Support /anime/create arg anime_id, episode_count when ANIME_BACKEND_ACTIVE use theese 2 args to generate dst_path

--- Controller/anime.py
+++ Controller/anime.py
@@ -4,11 +4,13 @@ from celery.result import AsyncResult
 import os
 from os import getenv as env
 import re
+import base64
 """
 {
     "src_path": "aaa/src/ccc.mp4",
     "dst_path": "aaa/stream",
-    "token": "123456"
+    "anime_id": "1",
+    "episode_count": "1"
 }
 """
 
@@ -16,8 +18,12 @@ import re
 def create():
     data = request.get_json(silent=True)
     if data is None:
-        return Response('{"message": "application/json needed"}',
-                        status=406, mimetype='application/json',)
+        return Response('{"message": "Json parse error"}',
+                        status=406, mimetype='application/json')
+
+    if 'src_path' not in data.keys() or type(data['src_path']) != str:
+        return Response('{"message": "src_path not set or not str"}',
+                        status=400, mimetype='application/json')
 
     data['src_path'], dangerous_cnt = re.subn(
         '([*|\"\\\']|\.\.\\/)', '', data['src_path'])
@@ -29,6 +35,21 @@ def create():
 #    if extension != '.mp4':
 #        return Response('{"message": "Only support .mp4"}',
 #                        status=501, mimetype='application/json')
+
+    if env('CCNS_AMIME_BACKEND_ACTIVE') and 'anime_id' in data.keys() and 'episode_count' in data.keys():
+        if type(data['anime_id']) != str:
+            return Response('{"message": "anime_id must be str"}',
+                            status=400, mimetype='application/json')
+        if type(data['episode_count']) != str:
+            return Response('{"message": "episode_count must be str"}',
+                            status=400, mimetype='application/json')
+
+        data['dst_path'] = data['anime_id'] + '/' + \
+            str(base64.urlsafe_b64encode((data['episode_count']).encode('utf-8')))
+
+    if 'dst_path' not in data.keys() or type(data['dst_path']) != str:
+        return Response('{"message": "dst_path not set or not str"}',
+                        status=400, mimetype='application/json')
 
     data['dst_path'], dangerous_cnt = re.subn(
         '([*|\"\\\']|\.\.\\/)', '', data['dst_path'])
