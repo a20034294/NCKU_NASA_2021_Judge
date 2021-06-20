49dbe020b497df49e7dcef470be0b90e001359c8
a20034294 <a20034294@gmail.com>
2021-06-20 22:27:26 +0800

    [Design] Use func decorator to simplify architecture

--- Task/judge_create.py
+++ Task/judge_create.py
@@ -4,6 +4,7 @@ from os import getenv as env
 import re
 
 students = {}
+result_data = {}
 with open(env('STUDENT_PATH'), 'r', encoding='utf-8') as f:
     lines = f.readlines()
     for line in lines:
@@ -11,6 +12,28 @@ with open(env('STUDENT_PATH'), 'r', encoding='utf-8') as f:
         students[word[0]] = (word[0], word[1], word[2])
 
 
+def judge_create_task(student_id, password):
+    result_data['result'] = {}
+    result_data['score'] = 0
+    ip = students[student_id][2]
+
+    chk_1a(ip)
+    chk_2a(ip)
+    return result_data
+
+
+def chk_wrap(score, problem_id):
+    def decorator(f):
+        def wrap(*args, **kwargs):
+            result_data['result'][problem_id] = 0
+            if (f(*args, **kwargs)):
+                result_data['result'][problem_id] = score
+                result_data['score'] = result_data['score'] + score
+        return wrap
+    return decorator
+
+
+@chk_wrap(10, '1a')
 def chk_1a(ip):
     ssh = f"ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no ncku-nasa@{ip} -t "
     script = f"{ssh}'snmpwalk -v 2c 127.0.0.1 -c \"public\" .1.3.6.1.4.1.2021.4'"
@@ -24,6 +47,7 @@ def chk_1a(ip):
     return False
 
 
+@chk_wrap(5, '2a')
 def chk_2a(ip):
     ssh = f"ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no ncku-nasa@{ip} -t "
     script = f"{ssh}'sudo netstat -tunlp'"
@@ -36,23 +60,3 @@ def chk_2a(ip):
     if re.search('[^\n]+:80\s.*nginx', result) is not None:
         return True
     return False
-
-
-def judge_create_task(student_id, password):
-    result_data = {}
-    result_data['result'] = {}
-    ip = students[student_id][2]
-
-    score = 0
-    result_data['result']['1a'] = 0
-    if chk_1a(ip):
-        result_data['result']['1a'] = 10
-        score = score + 10
-
-    result_data['result']['2a'] = 0
-    if chk_2a(ip):
-        result_data['result']['2a'] = 5
-        score = score + 5
-
-    result_data['score'] = score
-    return result_data
